6ecf12731564a4b4592eeb4a2710ce6e4900fe14
Tiago Kretzer Walter <tiagokwalter@gmail.com>
2024-05-27 14:10:07 -0300

    prepare to deploy to heroku

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import random
-import eventlet
-
-eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
